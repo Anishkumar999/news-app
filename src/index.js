3cc30fe21c95e662792e0975b1c0b4f705c79724
Anishkumar999 <anishkumar6667@gmail.com>
2024-10-02 02:34:31 +0530

    a

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HasRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { store } from './redux/store/store';
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HasRouter> 
+    <HashRouter> 
     <Provider store={store}>
       <App/>
     </Provider>
-    </HasRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
