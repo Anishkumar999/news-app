b57a81dcd25d2646803d72f10f68349d12a57e75
Anishkumar999 <anishkumar6667@gmail.com>
2024-10-08 14:16:11 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -10,11 +10,9 @@ import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='news-app'> 
     <Provider store={store}>
       <App/>
     </Provider>
-    </BrowserRouter>
   </React.StrictMode>
 );
 
